a0e61ba97fd749f9aaaddfb702a1db3a6bd28fb9
sablanchard <sablanchard@berkeley.edu>
2017-03-13 20:48:57 -0700

    downgraded numpy version requirement

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         'requests >= 2.9.1',
         'pandas >= 0.16.0',
-        'numpy>=1.11',
+        'numpy>=1.10',
         'geopandas>=0.2.1',
         'Shapely>=1.5'
     ]
