1e2d762386c42156ef2dd8d47c93c3a27ac57001
Federico J. Fernandez <ffernandez@synthicity.com>
2017-03-31 12:52:34 -0300

    Changing reference to README file
    Since the README file is used as the long description, that should be
    coordinated with the actual file format used (and its extension).

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 
 # read README as the long description
-with open('README.md', 'r') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
