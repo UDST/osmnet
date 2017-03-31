1dcd78b56a3248e3a5c5b3d3b52dc2fe2828efba
Federico J. Fernandez <ffernandez@synthicity.com>
2017-03-31 13:00:32 -0300

    Increasing version after fix
    New release after the MD to RST change in the README file.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='osmnet',
-    version='0.1.0',
+    version='0.1.1',
     license='AGPL',
     description=('Tools for the extraction of OpenStreetMap street network '
                  'data for use in Pandana accessibility analyses.'),
